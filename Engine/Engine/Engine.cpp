df7dd034b03d4eb82dbdf903639303379ff28f71
JVP11292021 <jessyvanpolanen@gmail.com>
2025-11-19 17:03:48 +0100

    update: made cool pattern

--- Engine/Engine/Engine.cpp
+++ Engine/Engine/Engine.cpp
@@ -39,13 +39,31 @@ public:
 		vkDeviceWaitIdle(this->device.device());
 	}
 private:
+	void sierpinski(
+		std::vector<vle::ShaderModel::Vertex>& vertices,
+		int depth,
+		glm::vec2 left,
+		glm::vec2 right,
+		glm::vec2 top
+	) {
+		if (depth <= 0) {
+			vertices.push_back({ top });
+			vertices.push_back({ right });
+			vertices.push_back({ left });
+		}
+		else {
+			auto leftTop = 0.5f * (left + top);
+			auto rightTop = 0.5f * (right + top);
+			auto leftRight = 0.5f * (left + right);
+			sierpinski(vertices, depth - 1, left, leftRight, leftTop);
+			sierpinski(vertices, depth - 1, leftRight, right, rightTop);
+			sierpinski(vertices, depth - 1, leftTop, rightTop, top);
+		}
+	}
+
 	void loadModels() {
-		std::vector<vle::ShaderModel::Vertex> vertices = {
-			{{0.0, -0.5f}},
-			{{0.5f, 0.5f}},
-			{{-0.5f, 0.5f}}
-		};
-		
+		std::vector<vle::ShaderModel::Vertex> vertices{};
+		sierpinski(vertices, 5, { -0.5f, 0.5f }, { 0.5f, 0.5f }, { 0.0f, -0.5f });
 		this->model = std::make_unique<vle::ShaderModel>(this->device, vertices);
 	}
 
